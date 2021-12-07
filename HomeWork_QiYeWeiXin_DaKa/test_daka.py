bd9425d08926721cf895fa161308c134b246787a
Administrator <727991861@qq.com>
2021-12-07 11:10:23 +0800

    appium-企业微信手机端-外出打卡

--- HomeWork_QiYeWeiXin_DaKa/test_daka.py
+++ HomeWork_QiYeWeiXin_DaKa/test_daka.py
@@ -1,6 +1,6 @@
 from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
 from selenium.webdriver.common.by import By
-
 
 class TestDaKa:
     def setup(self):
@@ -12,12 +12,12 @@ class TestDaKa:
             "noReset": True,
             "unicodeKeyBoard":True,
             "resetKeyBoard":True,
-
         }
         self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_cap)
         self.driver.implicitly_wait(15)
     def teardown(self):
-        self.driver.quit()
+        #self.driver.quit()
+        pass
 
     def test_daka(self):
         '''
@@ -29,4 +29,14 @@ class TestDaKa:
         验证点：提示【外出打卡成功】
         :return:
         '''
-        self.driver.find_element(By.XPATH)
+        #定位并点击 工作台，组合查询：resource id 和 text
+        self.driver.find_element_by_android_uiautomator('new UiSelector().resourceId("com.tencent.wework:id/f3a").text("工作台")').click()
+        #定位并点击 打卡
+        self.driver.find_element(MobileBy.XPATH,'//*[@text="打卡"]').click()
+        #定位并点击 外出打卡
+        self.driver.find_element(MobileBy.XPATH, '//*[@text="外出打卡"]').click()
+        #定位并点击 包含“次打卡”的元素
+        self.driver.find_element_by_android_uiautomator('new UiSelector().textContains("次外出")').click()
+
+        #验证打卡成功：定位“外出打卡成功”，并打印
+        print(self.driver.find_element(MobileBy.XPATH, '//*[@text="外出打卡成功"]').text)
